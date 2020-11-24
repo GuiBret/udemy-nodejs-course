de280c54891ed9d9af070f2c316bf41199554334
GuiBret <guillaume.bretzner@gmail.com>
2020-11-24 21:16:18 +0100

    Handle routing

--- section3/server.js
+++ section3/server.js
@@ -3,7 +3,18 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers); 
+
+    const url = req.url;
+
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" /><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -11,6 +22,8 @@ const server = http.createServer((req, res) => {
     res.write('<body><h1>Hello</h1></body>');
     res.write('</html>');
     res.end();
+
+    
 });
 
 server.listen(4000);
