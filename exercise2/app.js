8ef8c2e99047619d248fa28f4f2abb4130c844c4
GuiBret <guillaume.bretzner@gmail.com>
2020-11-29 14:58:22 +0100

    Handle / and /users from prev assignment

--- exercise2/app.js
+++ exercise2/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use('/', (req, res, next) => {
+app.use('/users', (req, res, next) => {
     console.log('Hello from the first middleware');
-    next();
+
+    
+    res.send('<h1>My user list</h1><ul><li>User 1</li><li>User 2</li></ul>');
+
 });
 
 app.use('/', (req, res, next) => {
     console.log('Hello from the second middleware');
-    res.send('<h1>Hello from the response of the second middleware</h1>');
-})
+    
+    res.send('<html><body><h1>Hello from my Express server</h1><form method="POST" action="/create-user"><input type="text" name="username" /><input type="submit"/></form></body></html>');
+    
+});
+
+app.listen(3000);
+
