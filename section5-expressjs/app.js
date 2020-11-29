d76f04730b91c18c61183dc6f69d5869dbb1cc9d
GuiBret <guillaume.bretzner@gmail.com>
2020-11-29 14:51:11 +0100

    Handle different routes

--- section5-expressjs/app.js
+++ section5-expressjs/app.js
@@ -1,19 +1,18 @@
-const http = require('http');
+
 
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware !');
-    next();
-});
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware 23!');
+    res.send('<h1>Hello from the "Add product" page ! </h1>')
+
+});
+app.use('/', (req, res, next) => {
     console.log('In the middleware 23!');
     res.send('<h1>Hello from Express ! </h1>')
 
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
