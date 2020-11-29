fe10d9ed68155182db2b94ba60db234569f1ddc0
GuiBret <guillaume.bretzner@gmail.com>
2020-11-29 12:45:35 +0100

    Registers / and /users

--- exercise1/routes.js
+++ exercise1/routes.js
@@ -2,9 +2,16 @@ const routesHandler = (req, res) => {
     const url = req.url;
     const method = req.url;
 
+    res.setHeader('Content-Type', 'text/html');
+    // Sends back a simple greeting
     if(url === '/') {
-        
+        res.write('<h2>Hello from my Node server</h2>');
+    } else if(url === '/users') { // Sends a dummy user list
+        res.write('<h1>My user list</h1>');
+        res.write('<ul><li>User 1</li><li>User 2</li></ul>');
     }
+
+    res.end();
 
 }
 
