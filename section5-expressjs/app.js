a5c9529c111d53730b6e6ff54766659d1c7b5741
GuiBret <guillaume.bretzner@gmail.com>
2020-11-30 19:53:48 +0100

    Create & handle 404

--- section5-expressjs/app.js
+++ section5-expressjs/app.js
@@ -4,6 +4,7 @@ const shopRoutes = require('./routes/shop');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -12,7 +13,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
