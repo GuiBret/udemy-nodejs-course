a654e68e220ae03ee5a5a5636c4d66bf2baa0717
GuiBret <guillaume.bretzner@gmail.com>
2020-11-30 19:45:34 +0100

    Handle subroutes

--- section5-expressjs/routes/admin.js
+++ section5-expressjs/routes/admin.js
@@ -5,10 +5,10 @@ const router = express.Router();
 router.get('/add-product', (req, res, next) => {
     
     // res.send('<h1>Hello from the "Add product" page ! </h1>');
-    res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send('<form method="POST" action="/admin/add-product"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 
 });
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
     console.log(req.body);
     res.redirect('/');
 });
