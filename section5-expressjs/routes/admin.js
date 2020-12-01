e5a08791927100f033a14e4179be008f36a0a728
GuiBret <guillaume.bretzner@gmail.com>
2020-12-01 18:56:13 +0100

    Add & use helper function

--- section5-expressjs/routes/admin.js
+++ section5-expressjs/routes/admin.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     
     // res.send('<h1>Hello from the "Add product" page ! </h1>');
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 
 });
 router.post('/add-product', (req, res) => {
