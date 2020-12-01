c470071045154f9aca40643c1dca93088abbb5bb
GuiBret <guillaume.bretzner@gmail.com>
2020-12-01 19:16:03 +0100

    Step 2 : add 2 routes & serve html files

--- exercise3/app.js
+++ exercise3/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+
+app.get('/users', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'users.html'));
+});
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
 
 
 app.listen(3001);
