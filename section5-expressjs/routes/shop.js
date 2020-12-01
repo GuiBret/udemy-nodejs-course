08b4762de09346348e37c5de6bd1a4025b25bd83
GuiBret <guillaume.bretzner@gmail.com>
2020-12-01 19:48:26 +0100

    "Persist" data

--- section5-expressjs/routes/shop.js
+++ section5-expressjs/routes/shop.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const adminData = require('../admin');
 
 router.get('/', (req, res, next) => {
     
