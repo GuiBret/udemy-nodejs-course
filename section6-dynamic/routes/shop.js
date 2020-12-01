57a145b5a2c56fbdb073f88176550191638ada71
GuiBret <guillaume.bretzner@gmail.com>
2020-12-01 20:33:22 +0100

    Add data binding to pug template

--- section6-dynamic/routes/shop.js
+++ section6-dynamic/routes/shop.js
@@ -6,12 +6,13 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 const adminData = require('./admin');
+const products = adminData.products;
 
 router.get('/', (req, res, next) => {
   console.log(adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 
-  res.render('shop');
+  res.render('shop', {prods: products, docTitle: "Shop"});
 });
 
 module.exports = router;
