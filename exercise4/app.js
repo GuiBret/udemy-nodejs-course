46a927a269c324856b27c8bc87a3ff5368cb21f6
GuiBret <guillaume.bretzner@gmail.com>
2020-12-06 12:21:10 +0100

    Add routes + push user

--- exercise4/app.js
+++ exercise4/app.js
@@ -1,15 +1,32 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 
 const app = express();
+const users = [];
 
+// Add bodyparser for POST /add-user
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Add EJS
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// app.get('/', (req, res) => {
-//     res.send('Coucou');
-// });
-// app.post('/add-user', (req, res) => {
-//     res.redirect('/');
-// });
+app.get('/', (req, res) => {
+    
+    res.render('index');
+});
+
+app.get('/users', (req, res) => {
+    res.render('users');
+});
+
+app.post('/add-user', (req, res) => {
+    let username = req.body.username;
+    users.push(username);
+    res.redirect('/');
+});
 
 app.listen(3000);
