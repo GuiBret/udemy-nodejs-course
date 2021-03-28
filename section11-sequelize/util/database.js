487589c6da915b6958863f0abd3b5a145b0c56b8
GuiBret <guillaume.bretzner@gmail.com>
2021-03-28 19:40:23 +0200

    Edit sequelize config

--- section11-sequelize/util/database.js
+++ section11-sequelize/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mydb', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('mydb', 'user', 'password', {
     dialect: 'mysql',
     host: 'localhost'
 });
