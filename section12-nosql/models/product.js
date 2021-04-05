2780ef336c7c7d871b90e63fa8d754e2403f6bc9
GuiBret <guillaume.bretzner@gmail.com>
2021-04-05 19:52:35 +0200

    Handle userId in product

--- section12-nosql/models/product.js
+++ section12-nosql/models/product.js
@@ -2,12 +2,13 @@ const getDB = require('../util/database').getDB;
 const mongodb = require('mongodb');
 const ObjectId = mongodb.ObjectId;
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new ObjectId(id) : null;
+    this.userId = userId
   }
 
   save() {
