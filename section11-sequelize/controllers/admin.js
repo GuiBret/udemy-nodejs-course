58168c35138c0a72ed39c62294297611378210e1
GuiBret <guillaume.bretzner@gmail.com>
2021-04-05 08:54:34 +0200

    Fix functions in product controller

--- section11-sequelize/controllers/admin.js
+++ section11-sequelize/controllers/admin.js
@@ -13,7 +13,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+
+  req.user.createProduct({
     title: title,
     description: description,
     price: price,
@@ -31,15 +32,18 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
-    if (!product) {
+
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+          .then(products => {
+    if (!products[0]) {
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product
+      product: products[0]
     });
   }).catch(err => console.log(err));
 };
@@ -66,7 +70,7 @@ exports.postEditProduct = async(req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products => {
+  req.user.getProducts().then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
