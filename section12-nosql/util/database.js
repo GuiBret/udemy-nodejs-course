ba1e74e3df4e21e0d67a7bc6c473eb2b2fc982ba
GuiBret <guillaume.bretzner@gmail.com>
2021-04-05 10:49:10 +0200

    Create getDB function, lets us access the DB more easily

--- section12-nosql/util/database.js
+++ section12-nosql/util/database.js
@@ -1,15 +1,23 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (_cb) => {
-  MongoClient.connect('mongodb+srv://nodejsuser:nodejspassword@cluster0.a7jln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  MongoClient.connect('mongodb+srv://nodejsuser:nodejspassword@cluster0.a7jln.mongodb.net/shop?retryWrites=true&w=majority')
              .then(result => {
-               console.log('Connected');
+               _db = client.db();
+
                _cb(result);
              })
              .catch(err => console.log(err));
 }
 
+const getDB = () => {
+  if(_db) {
+    return _db;
+  }
+}
 
-
-module.exports = mongoConnect;
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
